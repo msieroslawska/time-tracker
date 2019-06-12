5e4b7fd76dd48170db86341679b184e0817d2626
Marta <msieroslawska@users.noreply.github.com>
2019-06-12 17:48:42 +0200

    More elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="main">
-      <section className="add">
-        <h1 className="header">Add a new task</h1>
-        <input
-          className="new-task-input"
-          name="new-task"
-          type="text"
-        />
-      </section>
+class App extends React.Component {
+  state = {
+    inputValue: '',
+    taskList: [],
+  }
 
-      <section className="task-list">
+  handleAddTask = () => {
+    const { inputValue } = this.state;
+    const newList = this.state.taskList.push(inputValue);
+    this.setState({
+      taskList: newList
+    })
+  }
 
-      </section>
-    </div>
-  );
+  handleInputChange = (ev) => {
+    this.setState({
+      inputValue: ev.target.value
+    });
+  }
+
+  renderTaskList = () => {
+    const { taskList } = this.state;
+    return taskList.map(task => (
+      <div>{task}</div>
+    ))
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <section className="add">
+          <h1 className="header">Add a new task</h1>
+          <input
+            className="new-task-input"
+            onChange={this.handleInputChange}
+            name="new-task"
+            type="text"
+          />
+          <button
+            type="button"
+            onClick={this.handleAddTask}
+          >
+            Add new task
+          </button>
+        </section>
+
+        <section className="task-list">
+          {this.renderTaskList()}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
