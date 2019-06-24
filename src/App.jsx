51c0f4e7a6ff4f9724fd56d7678f537a2e199f66
Marta <msieroslawska@users.noreply.github.com>
2019-06-24 14:12:03 +0200

    Use localStorage to save task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 
 class App extends React.Component {
-  state = {
-    inputValue: '',
-    taskList: [],
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      taskList: this.getLocalStorageTasks(),
+    };
   }
 
+  getLocalStorageTasks = () => JSON.parse(localStorage.getItem('taskList')) || []
+
   handleAddTask = () => {
-    const { inputValue } = this.state;
-    const newList = this.state.taskList.push(inputValue);
+    const { inputValue, taskList } = this.state;
+    if (inputValue !== '') {
+      taskList.push(inputValue);
+      this.setState({
+        inputValue: '',
+        taskList,
+      });
+      localStorage.setItem('taskList', JSON.stringify(taskList));
+    }
+  }
+
+  handleRemoveAllTasks = () => {
     this.setState({
-      taskList: newList
-    })
+      inputValue: '',
+      taskList: [],
+    });
+    localStorage.removeItem('taskList');
   }
 
   handleInputChange = (ev) => {
     this.setState({
-      inputValue: ev.target.value
+      inputValue: ev.target.value,
     });
   }
 
   renderTaskList = () => {
     const { taskList } = this.state;
     return taskList.map(task => (
-      <div>{task}</div>
+      <div key={this.generateKey(task)}>{task}</div>
     ))
   }
+
+  generateKey = (pre) => `${pre}-${ new Date().getTime() }`
 
   render() {
     return (
@@ -37,12 +56,21 @@ class App extends React.Component {
             onChange={this.handleInputChange}
             name="new-task"
             type="text"
+            value={this.state.inputValue}
           />
+
           <button
             type="button"
             onClick={this.handleAddTask}
           >
             Add new task
+          </button>
+
+          <button
+            type="button"
+            onClick={this.handleRemoveAllTasks}
+          >
+            Clear the task list
           </button>
         </section>
 
